5135037b92d4d2ccaf35d395ab2f65df2fcc24f9
Aschkor <aschkor@protonmail.com>
2019-10-13 14:53:24 +0100

    Add script to install dependency

---
+++ install_py_dependency.py
@@ -0,0 +1,8 @@
+from pip._internal import main
+
+def install_module(mod):
+    main(["install","--user",mod])
+
+if __name__ == "__main__":
+    install_module("appdirs")
+    install_module("gitpython")
