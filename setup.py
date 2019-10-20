247340bd65ee37ba2668bc56ee0938d47bed47e2
aschkor <aschkor@protonmail.com>
2019-10-20 18:25:12 -0400

    add input argument
    For chose the soft (vim or neovim), the user need to specify the argumentwq

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import argparse
+
 from install.install import install
 from install.var import var_neovim
 
+
+def parse():
+    parser =argparse.ArgumentParser(description='Install vim or neovim with packages as user')
+    parser.add_argument('soft', help = 'choose vim or neovim to install')
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    global_var = var_neovim()
-    install(global_var)
+    
+    parser = parse()
+    
+    if parser.soft == 'neovim':
+        global_var = var_neovim()
+        install(global_var)
+    elif parser.soft == 'vim':
+        pass
+
 
